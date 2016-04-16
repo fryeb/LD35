70634fb743ec319907322a7b2c284c2c34d70cc7
Benjamin Frye <ben.j.frye@gmail.com>
2016-04-16 14:10:27 +1000

    Added Circle.png

---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.0f;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButton(KeyCode.W))
+        {
+
+        }
+	}
+}
