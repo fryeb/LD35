26ada7161b965e1d9237c325598578e2f52b7ddb
Benjamin Frye <ben.j.frye@gmail.com>
2016-04-18 07:12:17 +1000

    Done more stuff than I can count

---
+++ Assets/Bosses/Boss Five/Octopus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Octopus : Boss
+{
+    public int numBarrels;
+    private int currentBarrel;
+
+    void Update()
+    {
+        if (timer <= 0)
+        {
+
+            Transform bullet = Instantiate(bulletPrefab, transform.position, Quaternion.AngleAxis(90, Vector3.left)) as Transform;
+            Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            timer = 1 / fireSpeed;
+        }
+        else timer -= Time.deltaTime;
+    }
+
+}
